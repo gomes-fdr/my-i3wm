c7eacd5d5a233d1e9ca7712acfdbf6a85bf844df
Fabiano da Rosa Gomes <gomes.fdr@gmail.com>
2020-02-16 21:14:59 -0300

    Para uma instalação minima e organizada do dwm

--- dwm.sh
+++ dwm.sh
@@ -1,10 +1,13 @@
 sudo apt update
 sudo apt upgrade
 
-sudo apt install -f -y curl wget dirmngr git
-sudo apt install -f -y xorg fonts-font-awesome fonts-roboto fonts-moto fonts-mplus xgd-user-dirs
-sudo apt install -f -y slim
-sudo apt install -f -y dwm suckless-tools stterm
+sudo apt install -f -y curl wget git
+sudo apt install -f -y xorg xgd-user-dirs
+sudo apt install -f -y make gcc libx11-dev libxft-dev libxinerama-dev xorg
+#sudo apt install -f -y fonts-font-awesome fonts-roboto fonts-moto fonts-mplus xgd-user-dirs
+#sudo apt install -f -y slim
+
+# Alguns apps iniciais
 sudo apt install -f -y firefox-esr
 sudo apt install -f -y file-roller pcmanfm neofetch geany
 
@@ -12,8 +15,10 @@ sudo apt install -f -y file-roller pcmanfm neofetch geany
 xdg-user-dirs-update
 mkdir projects
 
-## my dot files
-git clone https://github.com/gomes-fdr/dotfiles ~/projects
+## DWM last version
+git clone https://git.suckless.org/dwm ~/projects
+git clone https://git.suckless.org/st ~/projects
+git clone https://git.suckless.org/dmenu ~/projects
 
 ## install oh-my-bash
 sh -c "$(curl -fsSL https://raw.github.com/ohmybash/oh-my-bash/master/tools/install.sh)"
